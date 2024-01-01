1d7957940453877e3873fe71faeb64cd14410ae4
abid-sayyad <sayyad.abid16@gmail.com.com>
2024-01-01 17:52:32 +0530

    Printing different messages for odd/ even PIDs)

--- hello-ring-buf.py
+++ hello-ring-buf.py
@@ -8,17 +8,28 @@ program = r"""
         int uid;
         char command[16];
         char message[12];
+        char message2[12];
     };
 
     int hello(void *ctx) {
         struct data_t data = {};
         char message[11] = "Hello Abid";
+        char message2[11] = "Hello Baby";
     
         data.pid = bpf_get_current_pid_tgid() >> 32;
         data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
     
         bpf_get_current_comm(&data.command, sizeof(data.command));
-        bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
+        
+        if((data.pid)%2 == 0)
+            bpf_probe_read_kernel(&data.message, sizeof(data.message), message);
+            
+        else
+            bpf_probe_read_kernel(&data.message, sizeof(data.message), message2);
+
+            
+        //uncomment while using user space Odd/ Even classification
+        //bpf_probe_read_kernel(&data.message2, sizeof(data.message2), message2);
 
         output.perf_submit(ctx, &data, sizeof(data));
 
@@ -31,8 +42,18 @@ b.attach_kprobe(event=syscall, fn_name="hello")
 
 def print_event(cpu, data, size):
     data = b["output"].event(data)
+
     print(f"{data.pid} {data.uid} {data.command.decode()}" + \
-          f"{data.message.decode()}")
+            f" { data.message.decode()}")
+
+
+    #User space program for printing different messages for odd and even PIDs
+    #if((data.pid)%2 == 0):
+    #    print(f"{data.pid} {data.uid} {data.command.decode()}" + \
+    #            f" { data.message.decode()}")
+    #else:
+    #     print(f"{data.pid} {data.uid} {data.command.decode()}" + \
+    #            f" { data.message2.decode()}")
 
 b["output"].open_perf_buffer(print_event)
 while True:
